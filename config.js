60924b9fe5a3b1ac6da2c4334b67da89b9ed141c
unknown <hoangduynhat0204@gmail.com>
2022-05-03 00:40:05 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Hương à, công chúa của anh!',
+    introDesc: `Anh không phải yêu em từ cái nhìn đầu tiên.
+    Nhưng từ lần tiếp theo, anh đã muốn lo cho em cả đời rồi.😘
+    Anh đã sắp xếp ổn thỏa mọi thứ.
+    Giờ là đến lúc anh bước tới bên em <3`,
+    btnIntro: 'Đi tiếp xem nào!',
+    title: 'Phải chăng em đã yêu anh ngay từ cái nhìn đầu tiên, phải không Hương ơi 😙',
+    desc: 'Em có đồng ý làm người yêu anh không?',
+    btnYes: 'Em đồng ý 😘',
+    btnNo: 'Không, Em muốn quay xe',
+    question:'Anh không có gì hết, chỉ dám hứa anh sẽ dành mọi thứ tốt đẹp nhất của anh cho em. Vậy sao em lại yêu anh? <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Anh sẽ làm em trở thành công chúa hạnh phúc nhất 😘😘',
+    messDesc: 'Ra ngoài đi, anh đứng đợi ngoài rồi này!!',
+    btnAccept: 'Okiii nè <3',
+    messLink: 'https://www.facebook.com/hoangduynhat0204/'
+}
